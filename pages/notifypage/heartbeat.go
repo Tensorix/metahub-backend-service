65f6681f950acfc934c9ef2be95af49d0d8b3612
topwuther <topwuther@gmail.com>
2024-12-01 12:15:15 +0000

    make heartbeat workable

--- pages/notifypage/heartbeat.go
+++ pages/notifypage/heartbeat.go
@@ -1,29 +1,43 @@
 package notifypage
 
 import (
+	"log"
 	"time"
 
 	protov1 "github.com/Tensorix/metahub-backend-service/gen/proto/v1/auth"
 	notify "github.com/Tensorix/metahub-backend-service/gen/proto/v1/notify"
+	"github.com/Tensorix/metahub-backend-service/pages/authpage"
 	"google.golang.org/grpc"
 )
 
-var (
-	interval = 5 * time.Second
-)
+var interval = 5 * time.Second
 
 func (s *server) Heartbeat(in *notify.HeartbeatRequest, stream grpc.ServerStreamingServer[notify.HeartbeatResponse]) error {
-	response := notify.HeartbeatResponse{
-		Result: protov1.CheckResult_CHECK_RESULT_FAILED,
-		Details: []*notify.Detail{
-			// {
-			// 	Connected:  true,
-			// 	AccountTag: "tag1",
-			// },
-		},
-		Interval: int32(interval / time.Millisecond),
-	}
+	bots := authpage.Bots
+	token := in.Token
+	username := authpage.GetUsername(token)
 	for {
+		var details []*notify.Detail
+		for _, bot := range bots {
+			if bot == nil {
+				break
+			}
+			log.Println("Avaliable:",bot.Avaliable())
+			log.Println("AccountTag:",bot.AccountTag)
+			if bot.Username == username {
+				detail := &notify.Detail{
+					Connected:  bot.Avaliable(),
+					AccountTag: bot.AccountTag,
+				}
+				details = append(details, detail)
+			}
+		}
+		response := notify.HeartbeatResponse{
+			Result:   protov1.CheckResult_CHECK_RESULT_SUCCESS,
+			Details:  details,
+			Interval: int32(interval / time.Millisecond),
+		}
+
 		err := stream.Send(&response)
 		if err != nil {
 			return err
